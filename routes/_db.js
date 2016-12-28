0eda1f5b55353c8ec26bd427e91ddfa080a1ca46
isoares <soares.itamar@gmail.com>
2016-12-28 16:03:31 +1100

    mongodb_url

--- routes/_db.js
+++ routes/_db.js
@@ -2,19 +2,23 @@ var mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost/sumula');
 
-var connection_string = '127.0.0.1:27017/sumula';
+var connection_string = 'mongodb://127.0.0.1:27017/sumula';
 
 //if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-	connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-	process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-	process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-	process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-	process.env.OPENSHIFT_APP_NAME;
+//if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+//	connection_string = 'mongodb://' + 
+//	process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+//	process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//	process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//	process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//	process.env.OPENSHIFT_APP_NAME;
+//}
+if (process.env.MONGODB_URL) {
+	connection_string = process.env.MONGODB_URL + process.env.OPENSHIFT_APP_NAME; 
 }
 
 //var dbConn = mongoose.createConnection('mongodb://' + connection_string);
-mongoose.connect('mongodb://' + connection_string);
+mongoose.connect(connection_string);
 
 var dbConn = mongoose.connection;
 
